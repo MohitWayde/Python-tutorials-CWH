f314820bbfcceeea494a0cbe4803532aa717de27
MohitWayde <62068704+MohitWayde@users.noreply.github.com>
2020-12-28 19:30:35 +0530

    Update Practice-Problem-7-Search-Engine-lite.py

--- Python tuts CWH/Practice-Problem-7-Search-Engine-lite.py
+++ Python tuts CWH/Practice-Problem-7-Search-Engine-lite.py
@@ -1,18 +1,24 @@
-def search_engine_lite(query):
-    db = [
-        "python is not python snake",
-        "python is very good",
-        "python is trending programming language"]
-    for i in db:
-        if i.lower() in db:
-            print(i)
 
+def matchingWords(sentence1, sentence2):
+    words1 = sentence1.split(" ")
+    words2 = sentence2.split(" ")
+    # print(words1)
+    # print(words2)
+    score = 0
+    for w1 in words1:
+        for w2 in words2:
+            # print(f"{w1} is matching with {w2}")
+            if w1.lower() == w2.lower():
+                score += 1
+    return score
     
 
 
-
-
-
-print("!WELCOME TO THE SEARCH ENGINE LITE!")
-search_query = input("Search Here !")
-search_engine_lite(search_query)
+# matchingWords("python is good", "this is good")
+sentences = ["this is good","python is good", "python is great", "life long learner"]
+query = input("Search Here: ")
+score = [matchingWords(sentence, query) for sentence in sentences]
+sortedSentScore = [sentScore for sentScore in sorted(zip(score, sentences), reverse =True) if sentScore[0]!=0]
+print(f"{len(sortedSentScore)} results found!")
+for sc, resultquery in sortedSentScore:
+    print(f"{resultquery} : with the score of {sc}")
